44d6d1369049b562cb8d135aa0eb0c2aa26749af
Wil W <ww8668@gmail.com>
2021-09-28 11:32:40 +0800

    Add hotel data

--- data/hotels.tsx
+++ data/hotels.tsx
@@ -2,6 +2,9 @@ export const HOTELS = [
   {
     name: "Best Western Plus Hotel Hong Kong",
     address: "308 Des Voeux Road West",
+    website: "http://www.bestwesternplushotelhongkong.com/",
+    email: "hotel@bestwesternplushotel.com",
+    contactNumber: "34103333",
     coordinates: {
       lat: 22.287478531724673,
       lon: 114.13918537049629,
@@ -10,6 +13,9 @@ export const HOTELS = [
   {
     name: "CM+ Hotels & Serviced Apartments",
     address: "16 Connaught Road West",
+    website: "https://cmplus.com.hk/en/quarantine-package/",
+    email: "cmplus@cmpd.com.hk",
+    contactNumber: "35606738",
     coordinates: {
       lat: 22.28788453114779,
       lon: 114.14967762631642,
@@ -18,9 +24,370 @@ export const HOTELS = [
   {
     name: "Courtyard by Marriott Hong Kong",
     address: "167 Connaught Road West",
+    website: "http://www.courtyardhongkong.com/",
+    email: "cyhk@courtyard.com",
+    contactNumber: "37178888",
     coordinates: {
       lat: 22.28817551256935,
       lon: 114.13892439933228,
     }
   },
+  {
+    name: "Grand City Hotel",
+    address: "338 Queen's Road West",
+    website: "http://www.grandcityhotelhongkong.com/en/index.html",
+    email: "hotel@grandcityhotelhongkong.com",
+    contactNumber: "21921222",
+    coordinates: {
+      lat: 22.287034244663825,
+      lon: 114.14115457102399,
+    }
+  },
+  {
+    name: "JEN Hong Kong by Shangri-La",
+    address: "508 Queen's Road West",
+    website: "https://www.shangri-la.com/en/hotels/jen/hongkong/westerndistrict/",
+    email: "csteam.hongkong@shangri-la.com",
+    contactNumber: "27338786",
+    coordinates: {
+      lat: 22.286077215997636,
+      lon: 114.13497808213636,
+    }
+  },
+  {
+    name: "Lan Kwai Fong Hotel @ Kau U Fong",
+    address: "3 Kau U Fong",
+    website: "https://www.dcollection.com/en/lan-kwai-fong-hotel-kau-u-fong/",
+    email: "rsvn@lankwaifonghotel.com.hk",
+    contactNumber: "36500299",
+    coordinates: {
+      lat: 22.284420091159276,
+      lon: 114.15305348398822,
+    }
+  },
+  {
+    name: "One-Eight-One Hotel & Serviced Residences",
+    address: "181 Connaught Road West",
+    website: "https://www.one-eight-one.com/",
+    email: "enquiries@one-eight-one.com",
+    contactNumber: "31811688",
+    coordinates: {
+      lat: 22.287650704660713,
+      lon: 114.13673659748041,
+    }
+  },
+  {
+    name: "Ovolo Central",
+    address: "2 Arbuthnot Road",
+    website: "https://ovolohotels.com/quarantine-concierge-hong-kong/",
+    email: "reservations@ovologroup.com",
+    contactNumber: "21651000",
+    coordinates: {
+      lat: 22.280441269548856,
+      lon: 114.15463398398823,
+    }
+  },
+  {
+    name: "Ramada Hong Kong Harbour View",
+    address: "239 Queen's Road West",
+    website: "http://www.ramadahongkongharbourview.com/",
+    email: "rsvn@ramadahkhv.com",
+    contactNumber: "25999888",
+    coordinates: {
+      lat: 22.287121840784035,
+      lon: 114.14352486864442,
+    }
+  },
+  {
+    name: "The Landmark Mandarin Oriental Hong Kong",
+    address: "15 Queen's Road",
+    website: "https://www.mandarinoriental.com/hong-kong/the-landmark/luxury-hotel",
+    email: "anitangan@mohg.com",
+    contactNumber: "21320188",
+    coordinates: {
+      lat: 22.280726469123543,
+      lon: 114.15793899748026,
+    }
+  },
+
+  {
+    name: "Ramada Hong Kong Grand View",
+    address: "88 Chun Yeung Street, North Point",
+    website: "http://www.ramadahongkonggrandview.com/en/index.html",
+    email: "hotel@ramadahkgv.com",
+    contactNumber: "21303388",
+    coordinates: {
+      lat: 22.291269088696428,
+      lon: 114.19742795515235,
+    }
+  },
+
+  {
+    name: "Four Points by Sheraton Hong Kong",
+    address: "9 Yi Tung Road, Tung Chung, Lantau Island",
+    website: "https://www.marriott.com/offers/fun-filled-quarantine-stay-package-in-hong-kong-off-19568",
+    email: "fp.hkgfp.reservations@fourpoints.com",
+    contactNumber: "23528035",
+    coordinates: {
+      lat: 22.295500447082116,
+      lon: 113.94656045515241,
+    }
+  },
+  {
+    name: "Regal Airport Hotel",
+    address: "9 Cheong Tat Road, Hong Kong International Airport, Chek Lap Kok",
+    website: "https://www.regalhotel.com/regal-airport-hotel",
+    email: "https://www.regalhotel.com/regal-airport-hotel",
+    contactNumber: "22866888",
+    coordinates: {
+      lat: 22.31892316229653,
+      lon: 113.93435958398881,
+    }
+  },
+
+  {
+    name: "Bridal Tea House Hotel - Hung Hom",
+    address: "69 Gillies Avenue South, Hung Hom",
+    website: "https://www.bthhotel.com/",
+    email: "info@bthhotel.com",
+    contactNumber: "23641212",
+    coordinates: {
+      lat: 22.308394776152298,
+      lon: 114.18511015515264,
+    }
+  },
+  {
+    name: "iclub Ma Tau Wai Hotel",
+    address: "8 Ha Heung Road, Ma Tau Wai",
+    website: "https://www.iclub-hotels.com/iclub-ma-tau-wai-hotel",
+    email: "servicedesk@matauwai.iclub-hotels.com",
+    contactNumber: "39636600",
+    coordinates: {
+      lat: 22.316426116926888,
+      lon: 114.18879091282486,
+    }
+  },
+  {
+    name: "Kerry Hotel",
+    address: "38 Hung Luen Road, Hung Hom Bay, Kowloon",
+    website: "http://www.shangri-la.com/tc/hongkong/kerry/",
+    email: "csteam.hongkong@shangri-la.com",
+    contactNumber: "27338786",
+    coordinates: {
+      lat: 22.301393185610287,
+      lon: 114.1880385263167,
+    }
+  },
+  {
+    name: "Metropark Hotel Kowloon",
+    address: "75 Waterloo Road, Kowloon",
+    website: "https://hongkonghotel.metroparkhotelkowloon.com/eng/",
+    email: "hotel.hkkl@metroparkhotels.com",
+    contactNumber: "27611711",
+    coordinates: {
+      lat: 22.319640710875447,
+      lon: 114.17586472631692,
+    }
+  },
+  {
+    name: "Regal Oriental Hotel",
+    address: "30-38 Sa Po Road, Kowloon City",
+    website: "",
+    email: "roh.foam@regalhotel.com",
+    contactNumber: "61179319",
+    coordinates: {
+      lat: 22.330216946656176,
+      lon: 114.19296709748113,
+    }
+  },
+
+  {
+    name: "Dorsett Tsuen Wan",
+    address: "28 Kin Chuen Street, Kwai Chung",
+    website: "https://www.dorsetthotels.com/en/dorsett-tsuen-wan/offers/4_14_min16_nights_special.html",
+    email: "reservations.tsuenwan@dorsetthotels.com",
+    contactNumber: "39966863",
+    coordinates: {
+      lat: 22.370673713307855,
+      lon: 114.12952673980995,
+    }
+  },
+
+  {
+    name: "Nina Hotel Island South",
+    address: "55 Wong Chuk Hang Road, Aberdeen",
+    website: "https://www.ninahotelgroup.com/en/nina-hotel-island-south/quarantine",
+    email: "reservations.is@ninahotelgroup.com",
+    contactNumber: "39688838",
+    coordinates: {
+      lat: 22.249968325002886,
+      lon: 114.16820899933148,
+    }
+  },
+  {
+    name: "Ovolo Southside",
+    address: "64 Wong Chuk Hang Road, Wong Chuk Hang",
+    website: "https://ovolohotels.com/quarantine-concierge-hong-kong/",
+    email: "reservations@ovologroup.com",
+    contactNumber: "21651000",
+    coordinates: {
+      lat: 22.2498173051951,
+      lon: 114.16906428398771,
+    }
+  },
+  {
+    name: "Silka Far East Hotel",
+    address: "135-143 Castle Peak Road, Tsuen Wan, New Terrorites",
+    website: "https://www.silkahotels.com/en/silka-far-east/index.html",
+    email: "info.fareast@silkahotels.com",
+    contactNumber: "24069090",
+    coordinates: {
+      lat: 22.37116252712344,
+      lon: 114.11929031097391,
+    }
+  },
+  {
+    name: "Best Western Hotel",
+    address: "38 Bowrington Road, Causeway Bay",
+    website: "http://www.bestwesternhotelhongkong.com/en/index.html",
+    email: "rsvn@bestwesternhotelhongkong.com",
+    contactNumber: "24966666",
+    coordinates: {
+      lat: 22.277437774028026,
+      lon: 114.18074713980825,
+    }
+  },
+  {
+    name: "Crowne Plaza Hong Kong Causeway Bay",
+    address: "8 Leighton Road, Causeway Bay",
+    website: "https://www.cphongkong.com/en-us/",
+    email: "rsvn@cphongkong.com",
+    contactNumber: "39803980",
+    coordinates: {
+      lat: 22.277721123269405,
+      lon: 114.18268094627325,
+    }
+  },
+  {
+    name: "Dorsett Wanchai Hong Kong",
+    address: "387-397 Queen's Road East, Wan Chai",
+    website: "https://www.wanchai.dorsetthotels.com/",
+    email: "reservation.wanchai@dorsetthotels.com",
+    contactNumber: "35521133",
+    coordinates: {
+      lat: 22.274767478009796,
+      lon: 114.17891453980818,
+    }
+  },
+  {
+    name: "Eco Tree Hotel Causeway Bay",
+    address: "15 Mercury Street, Causeway Bay",
+    website: "https://causewaybay.ecotreehotel.com.hk/home/",
+    email: "reservation@ecotreehotelcwb.com.hk",
+    contactNumber: "23171788",
+    coordinates: {
+      lat: 22.285947506464844,
+      lon: 114.19228641282433,
+    }
+  },
+  {
+    name: "Hotel Indigo Hong Kong Island",
+    address: "246 Queen's Road East",
+    website: "http://www.hotelindigo.com/hongkong",
+    email: "rsvn.hkgin@ihg.com",
+    contactNumber: "39263888",
+    coordinates: {
+      lat: 22.274707503178295, 
+      lon: 114.17330176864412,
+    }
+  },
+  {
+    name: "Mira Moon Hotel",
+    address: "388 Jaffe Road, Causeway Bay",
+    website: "https://www.miramoonhotel.com/",
+    email: "enquiry@miramoonhotel.com",
+    contactNumber: "26438888",
+    coordinates: {
+      lat: 22.280014870184857,
+      lon: 114.17939343795643,
+    }
+  },
+  {
+    name: "Empire Hotel Causeway Bay",
+    address: "8 Wing Hing Street, Causeway Bay",
+    website: "http://www.empirehotel.com.hk/en/hotel/index",
+    email: "reservations-cwb@empirehotel-hongkong.com",
+    contactNumber: "36922333",
+    coordinates: {
+      lat: 22.28542205445152,
+      lon: 114.19213388398822,
+    }
+  },
+
+  {
+    name: "Pentahotel Hong Kong, Kowloon",
+    address: "19 Luk Hop Street, San Po Kong, Kowloon",
+    website: "https://www.pentahotels.com/hotels/asia/hong-kong-kowloon",
+    email: "reservations.kowloon@pentahotelsasia.com",
+    contactNumber: "31128222",
+    coordinates: {
+      lat: 22.337543484282147,
+      lon: 114.19963322631726,
+    }
+  },
+  {
+    name: "Bridal Tea House Hotel Yau Ma Tei",
+    address: "5 Wing Sing Lane, Yau Ma Tei, Kowloon",
+    website: "https://www.bthhotel.com/",
+    email: "info@bthhotel.com",
+    contactNumber: "37287000",
+    coordinates: {
+      lat: 22.311340674700052,
+      lon: 114.17061869748083,
+    }
+  },
+  {
+    name: "Dorsett Mongkok Hong Kong",
+    address: "88 Tai Kok Tsui Road, Kowloon",
+    website: "https://www.mongkok.dorsetthotels.com/",
+    email: "reservation.mongkok@dorsetthotels.com",
+    contactNumber: "39872388",
+    coordinates: {
+      lat: 22.321511182972774,
+      lon: 114.16215664166084,
+    }
+  },
+  {
+    name: "Ramada Hong Kong Grand",
+    address: "23 Austin Avenue, Tsim Sha Tsui, Kowloon",
+    website: "http://www.ramadahongkonggrand.com/en/index.html",
+    email: "rsvn@ramadahkg.com",
+    contactNumber: "31226222",
+    coordinates: {
+      lat: 22.303054633429785,
+      lon: 114.17470730390775,
+    }
+  },
+  {
+    name: "Sheraton Hong Kong Hotel and Towers",
+    address: "20 Nathan Road, Tsim Sha Tsui",
+    website: "https://www.marriott.com/hotels/travel/hkgsi-sheraton-hong-kong-hotel-and-towers/",
+    email: "RES.HongKong@sheraton.com",
+    contactNumber: "27326771",
+    coordinates: {
+      lat: 22.295376957302796,
+      lon: 114.17262097973472,
+    }
+  },
+  {
+    name: "Silka Seaview Hotel Hong Kong",
+    address: "268 Shanghai Street, Yau Ma Tei, Kowloon",
+    website: "https://www.silkahotels.com/en/silka-seaview/index.html",
+    email: "info.seaview@silkahotels.com",
+    contactNumber: "2782 0882",
+    coordinates: {
+      lat: 22.310448147684237,
+      lon: 114.17003989748073,
+    }
+  }
 ]
